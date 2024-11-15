12fa73094e919c6af5ca2cf8aa22d566d17df33a
Kristoffer Søholm <k.soeholm@gmail.com>
2024-11-15 22:53:49 +0100

    Use nightly for hot reloading

--- crates/database/seeds.sql
+++ crates/database/seeds.sql
@@ -2,8 +2,8 @@ BEGIN;
 
 delete from videos;
 
-insert into videos (title, youtube_id) values
-    ( 'Kjell Höglund Maskinerna är våra vänner', 'sZHL2EEXdiY' ),
-    ( 'Disco Snails | Vulfmon & Zachary Barker', 'oAZBKlVSLkU' );
+insert into videos (title, youtube_id, url) values
+    ( 'Kjell Höglund Maskinerna är våra vänner', 'sZHL2EEXdiY', ''),
+    ( 'Disco Snails | Vulfmon & Zachary Barker', 'oAZBKlVSLkU', '');
 
 COMMIT;
