016d1ca9ea2f5f435efbbcc4da11e26a57aaf062
Kristoffer Søholm <k.soeholm@gmail.com>
2024-11-16 01:32:38 +0100

    fix seeds

--- crates/database/seeds.sql
+++ crates/database/seeds.sql
@@ -2,8 +2,9 @@ BEGIN;
 
 delete from videos;
 
-insert into videos (title, youtube_id, url) values
-    ( 'Kjell Höglund Maskinerna är våra vänner', 'sZHL2EEXdiY', ''),
-    ( 'Disco Snails | Vulfmon & Zachary Barker', 'oAZBKlVSLkU', '');
+insert into videos (title, youtube_id, url, file_path) values
+    ( 'Shatner Of The Mount by Fall On Your Sword', 'HU2ftCitvyQ', '', 'HU2ftCitvyQ.mp4'),
+    ( 'Kjell Höglund Maskinerna är våra vänner', 'sZHL2EEXdiY', '', 'sZHL2EEXdiY.mp4'),
+    ( 'Disco Snails | Vulfmon & Zachary Barker', 'oAZBKlVSLkU', '', 'oAZBKlVSLkU.mp4');
 
 COMMIT;
